0e2f29334b8bf917f3230ea545e9c4e12221d68f
Gaspar Chefdeville <gaspar.chefdeville@gmail.com>
2023-06-04 18:30:36 +0200

    feat: create CharacterPage component

---
+++ src/components/Character/CharacterPage.tsx
@@ -0,0 +1,71 @@
+import { createUseStyles } from 'react-jss'
+import { CharacterInfos } from '../../types/CharacterInfos'
+import { Dungeon } from '../../types/MythicDungeon'
+import { FirstAffix } from '../../types/enums/FirstAffix'
+import Loader from '../common/Loader'
+import KeyPriorities from './KeyPriority/KeyPriorities'
+import MythicOverviewTable from './MythicOverview/MythicOverviewTable'
+import Profile from './Profile/Profile'
+import { RaiderApiScoreTiers } from '../../types/RaiderApi/RaiderApiScoreTiers'
+import Section from '../common/Section'
+
+const useStyles = createUseStyles({
+	container: {
+		display: 'flex',
+		flexDirection: 'column',
+		gap: 50
+	},
+	center: {
+		textShadow: '0 0 5px black',
+		minHeight: 70,
+		margin: [0, 'auto']
+	}
+})
+
+interface CharacterPageProps {
+	characterInfos?: CharacterInfos
+	weeklyAffix?: FirstAffix
+	currentSeasonDungeons: Dungeon[]
+	scoreTiers: RaiderApiScoreTiers[]
+	pending: boolean
+	error?: string
+}
+
+function CharacterPage ({ characterInfos, weeklyAffix, currentSeasonDungeons, scoreTiers, pending, error }: CharacterPageProps) {
+	const classes = useStyles()
+
+	if (pending) {
+		return (
+			<div className={classes.center}>
+				<Loader />
+			</div>
+		)
+	}
+
+	if (error != null || characterInfos == null || weeklyAffix == null || currentSeasonDungeons.length === 0) {
+		return (
+			<div className={classes.center}>
+				{error}
+			</div>
+		)
+	}
+
+	return (
+		<div className={classes.container}>
+			<Section title='Profile'>
+				<Profile characterInfos={characterInfos} scoreTiers={scoreTiers} />
+			</Section>
+
+			<Section title='Mythic overview'>
+				<MythicOverviewTable characterInfos={characterInfos} />
+			</Section>
+
+				<KeyPriorities
+					characterInfos={characterInfos}
+					weeklyAffix={weeklyAffix}
+				/>
+		</div>
+	)
+}
+
+export default CharacterPage
