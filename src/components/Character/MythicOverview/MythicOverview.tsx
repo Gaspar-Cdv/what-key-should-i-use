1d60750e503c804889ff4d334b2710a748aeaeb5
Gaspar Chefdeville <gaspar.chefdeville@gmail.com>
2023-06-04 22:17:32 +0200

    feat: use table component

--- src/components/Character/MythicOverview/MythicOverview.tsx
+++ src/components/Character/MythicOverview/MythicOverview.tsx
@@ -1,37 +1,21 @@
-import { createUseStyles } from 'react-jss'
 import { CharacterInfos } from '../../../types/CharacterInfos'
 import { FirstAffix } from '../../../types/enums/FirstAffix'
 import MythicOverviewHeaders from './MythicOverviewHeaders'
 import MythicOverviewRow from './MythicOverviewRow'
 import { useCallback } from 'react'
-
-const useStyles = createUseStyles({
-	table: {
-		tableLayout: 'fixed',
-		width: '100%',
-		color: 'white',
-		backgroundColor: '#000',
-		borderCollapse: 'collapse',
-		'& td,th': {
-			border: '1px solid  #333',
-			padding: [15, 20]
-		}
-	}
-})
+import Table from '../../common/Table'
 
 export const byTyrannicalFirst = (a: FirstAffix, b: FirstAffix) => a === FirstAffix.TYRANNICAL ? -1 : b === FirstAffix.TYRANNICAL ? 1 : 0
 
-export function byTyrannicalFirsts <T> (getter: (item: T) => FirstAffix) {
+export function byTyrannicalFirsts<T> (getter: (item: T) => FirstAffix) {
 	return (a: T, b: T) => getter(a) === FirstAffix.TYRANNICAL ? -1 : getter(b) === FirstAffix.TYRANNICAL ? 1 : 0
 }
 
-interface MythicOverviewProps {
+interface MythicOverviewTableProps {
 	characterInfos: CharacterInfos
 }
 
-function MythicOverview ({ characterInfos }: MythicOverviewProps) {
-	const classes = useStyles()
-
+function MythicOverview ({ characterInfos }: MythicOverviewTableProps) {
 	const getAllScoresByAffix = useCallback((affix: FirstAffix) => {
 		return characterInfos?.dungeons
 			.flatMap(dungeon => dungeon.runs.filter(run => run.affix === affix))
@@ -40,21 +24,21 @@ function MythicOverview ({ characterInfos }: MythicOverviewProps) {
 	}, [characterInfos])
 
 	return (
-		<table className={classes.table}>
-			<MythicOverviewHeaders />
-			<tbody>
-				{characterInfos.dungeons
-					.sort((a, b) => a.name.localeCompare(b.name))
-					.map(dungeon => (
-						<MythicOverviewRow
-							key={dungeon.shortName}
-							dungeon={dungeon}
-							getAllScoresByAffix={getAllScoresByAffix}
-						/>
-					))
-				}
-			</tbody>
-		</table>
+		<Table
+			headers={<MythicOverviewHeaders />}
+			fixedColumns
+		>
+			{characterInfos.dungeons
+				.sort((a, b) => a.name.localeCompare(b.name))
+				.map(dungeon => (
+					<MythicOverviewRow
+						key={dungeon.shortName}
+						dungeon={dungeon}
+						getAllScoresByAffix={getAllScoresByAffix}
+					/>
+				))
+			}
+		</Table>
 	)
 }
 
